d67b95d7e48b6d567b0a7b93dd8d6a97dd158595
yb70-7070 <smahlasvndy@gmail.com>
2025-06-27 16:52:26 +0200

    Update db.py

--- app/db.py
+++ app/db.py
@@ -4,6 +4,6 @@ def get_connection():
     return mysql.connector.connect(
         host="localhost",
         user="root",
-        password="Smahla70!",
+        password="",
         database="cloud_users"
     )
